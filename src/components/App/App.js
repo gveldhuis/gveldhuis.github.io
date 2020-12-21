3ae7eb5fb272f5049731a5f3a1fc85ca73b64056
gveldhuis <veldhuis@umich.edu>
2020-12-21 00:00:58 -0500

    Made NavBar and Intro components fully responsive

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 
 import { Intro } from '../Intro/Intro';
+import { NavBar } from '../NavBar/NavBar';
 /*import { NavBar } from '../NavBar/NavBar';
 import { AboutMe } from '../AboutMe/AboutMe';
 import { Experience } from '../Experience/Experience';
@@ -12,6 +13,7 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <NavBar />
         <Intro />
         {/*<NavBar />
         <AboutMe />
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,18 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NavBar.css';
+import '../../../src/index.css'
 
-export class NavBar extends React.Component {
-    render() {
-        return (
-            <div className="NavBar">
-                <h1 className='NavBar-nameLogo'>GRANT VELDHUIS</h1>
-                <ul className='NavBar-list'>
-                    <li>About Me</li>
-                    <li>Experience</li>
-                    <li>My Work</li>
-                    <li>Contact</li>
-                </ul>
+export function NavBar() {
+    const [click, setClick] = useState(false);
+
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+
+    const resizeMenu = () => {
+        if(window.innerWidth >= 1060) {
+            setClick(false);
+        }
+    };
+
+    window.addEventListener('resize', resizeMenu);
+
+    return (
+        <nav className="NavBar">
+            <h1 className='NavBar-nameLogo'>GRANT VELDHUIS</h1>
+            <div className='NavBar-menuicon' onClick={handleClick}>
+                <i className='fas fa-bars'></i>
             </div>
+            <div className={click ? 'NavBar-list-active' : 'NavBar-list'}>
+                <a href='' onClick={closeMobileMenu}>About Me</a>
+                <a href='' onClick={closeMobileMenu}>Experience</a>
+                <a href='' onClick={closeMobileMenu}>My Work</a>
+                <a href='' onClick={closeMobileMenu}>Contact</a>
+            </div>
+        </nav>
         );
-    }
 }
