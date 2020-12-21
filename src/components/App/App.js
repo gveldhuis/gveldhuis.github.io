80eefedd3fb0cdbcb84eb64b7ba57dbf2735bb8f
gveldhuis <veldhuis@umich.edu>
2020-12-21 17:38:20 -0500

    Made Skills component responsive

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,17 +2,22 @@ import React from 'react';
 import './App.css';
 
 
+import { Skills } from '../Skills/Skills';
 import { Experience } from '../Experience/Experience';
 import { AboutMe } from '../AboutMe/AboutMe';
 import { NavBar } from '../NavBar/NavBar';
 import { Intro } from '../Intro/Intro';
 
-
 /*
+
+
+
+
+
 import { AboutMe } from '../AboutMe/AboutMe';
 import { NavBar } from '../NavBar/NavBar';
 import { Intro } from '../Intro/Intro';
-import { Skills } from '../Skills/Skills';
+
 import { Work } from '../Work/Work';*/
 
 class App extends React.Component {
@@ -23,13 +28,12 @@ class App extends React.Component {
         <Intro />
         <AboutMe />
         <Experience />
+        <Skills />
+
 
         
         {/*
         
-        <NavBar />
-        <Intro />
-        <AboutMe />
         
         
         
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -5,41 +5,51 @@ export class Skills extends React.Component {
     render() {
         return (
             <div className='Skills'>
-                <h1 className='Skills-header'>Skills.</h1>
                 <div className='Skills-container'>
-                    <h2 className='Skills-type'>Languages &amp; Frameworks</h2>
-                    <h2 className='Skills-type'>Software Systems</h2>
-                    <h2 className='Skills-type'>Enterprise Applications</h2>
-                    <h2 className='Skills-type'>Soft Skills</h2>
-                    <ul className='Skills-list'>
-                        <li>JavaScript</li>
-                        <li>C++</li>
-                        <li>React.js</li>
-                        <li>Node.js</li>
-                        <li>HTML</li>
-                        <li>CSS</li>
-                        <li>mySQL</li>
-                    </ul>
-                    <ul className='Skills-list'>
-                        <li>Figma</li>
-                        <li>Sketch</li>
-                        <li>AnyLogic</li>
-                        <li>Power-BI</li>
-                        <li>Excel</li>
-                    </ul>
-                    <ul className='Skills-list'>
-                        <li>Microsoft Dynamics ERP</li>
-                        <li>Azure</li>
-                        <li>AWS</li>
-                    </ul>
-                    <ul className='Skills-list'>
-                        <li>Empathetic Leadership</li>
-                        <li>Product Management</li>
-                        <li>Cross-Collaboration</li>
-                        <li>Nonprofit Management</li>
-                        <li>Idea Synthesis</li>
-                    </ul>
-                </div>
+                    <h1 className='Skills-header'>Skills.</h1>
+                    <div className='Skills-block'>
+                        <div className='Skills-set'>
+                            <h2 className='Skills-type'>Languages &amp; Frameworks</h2>
+                            <ul className='Skills-list'>
+                                <li>JavaScript</li>
+                                <li>C++</li>
+                                <li>React.js</li>
+                                <li>Node.js</li>
+                                <li>HTML</li>
+                                <li>CSS</li>
+                                <li>mySQL</li>
+                            </ul>
+                        </div>
+                        <div className="Skills-set">
+                            <h2 className='Skills-type'>Software Systems</h2>
+                            <ul className='Skills-list'>
+                                <li>Figma</li>
+                                <li>Sketch</li>
+                                <li>AnyLogic</li>
+                                <li>Power-BI</li>
+                                <li>Excel</li>
+                            </ul>
+                        </div>
+                        <div className="Skills-set">
+                            <h2 className='Skills-type'>Enterprise Applications</h2>
+                            <ul className='Skills-list'>
+                                <li>Microsoft Dynamics</li>
+                                <li>Azure</li>
+                                <li>AWS</li>
+                            </ul>
+                        </div>
+                        <div className="Skills-set">
+                            <h2 className='Skills-type'>Soft Skills</h2>                      
+                            <ul className='Skills-list'>
+                                <li>Empathetic Leadership</li>
+                                <li>Product Management</li>
+                                <li>Cross-Collaboration</li>
+                                <li>Nonprofit Management</li>
+                                <li>Idea Synthesis</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>                
             </div>
         );
     }
