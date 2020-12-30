9df1ae45665c1098b320ca5f9bb9e20eddc5fe6e
gveldhuis <veldhuis@umich.edu>
2020-12-30 14:06:29 -0500

    Added JS interactivity funcs to NavBar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,30 +3,67 @@ import './NavBar.css';
 import '../../../src/index.css'
 
 export function NavBar() {
-    const [click, setClick] = useState(false);
 
-    const handleClick = () => setClick(!click);
+    /* Click state, top of page state */
+    const [click, setClick] = useState(false);
+    const [top, setTop] = useState(true);
+
+
+    /* Function that closes mobile side-menu */
     const closeMobileMenu = () => setClick(false);
 
+    /*Function that closes mobile menu when window width is greater than 1060px */
     const resizeMenu = () => {
         if(window.innerWidth >= 1060) {
             setClick(false);
         }
     };
 
+    /* Toggles color of navmenu when scrolling position changes */
+    const toggleNavMenu = () => {
+        if(document.documentElement.scrollTop < 50) {
+            setTop(true);
+        } else {
+            setTop(false);
+        }
+    }
+
+    /*//////////////////////////////////////////////////////////////////////*/
+
+    /* Handler functions */
+    const handleMobileClick = () => setClick(!click);
+
+    function handleLinkClick(event) {
+        event.preventDefault();
+
+        closeMobileMenu();
+
+        const href = event.target.getAttribute("href");
+        const offsetTop = document.querySelector(href).offsetTop;
+        
+        window.scrollTo({
+            top: offsetTop,
+            behavior: "smooth",
+        })
+    }
+
+    /*//////////////////////////////////////////////////////////////////////*/
+
+    /* Event listeners */
     window.addEventListener('resize', resizeMenu);
+    window.addEventListener('scroll', toggleNavMenu);
 
     return (
         <nav className="NavBar">
             <h1 className='NavBar-nameLogo'>GRANT VELDHUIS</h1>
-            <div className='NavBar-menuicon' onClick={handleClick}>
+            <div className='NavBar-menuicon' onClick={handleMobileClick}>
                 <i className='fas fa-bars'></i>
             </div>
             <div className={click ? 'NavBar-list-active' : 'NavBar-list'}>
-                <a href='#AboutMe' onClick={closeMobileMenu}>About Me</a>
-                <a href='' onClick={closeMobileMenu}>Experience</a>
-                <a href='' onClick={closeMobileMenu}>My Work</a>
-                <a href='' onClick={closeMobileMenu}>Contact</a>
+                <a href='#AboutMe' onClick={handleLinkClick}>About Me</a>
+                <a href='' onClick={handleLinkClick}>Experience</a>
+                <a href='' onClick={handleLinkClick}>My Work</a>
+                <a href='' onClick={handleLinkClick}>Contact</a>
             </div>
         </nav>
         );
