605f767143b5525f33a59035c515a341629bc6ee
gveldhuis <veldhuis@umich.edu>
2020-12-31 17:47:18 -0500

    Made Intro component switch between roles

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -3,7 +3,16 @@ import './Intro.css';
 import '../../../src/index.css'
 /*import introImage from './introPhoto-2.jpg';*/
 
+let rolesArray = ["NONPROFIT COO", "SOFTWARE DEVELOPER", "TENNIS PLAYER", "CS STUDENT"];
+
+
 export class Intro extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            roleIndex: 0,
+        }
+    }
     render() {
         return (
             <>
@@ -12,10 +21,18 @@ export class Intro extends React.Component {
                     <div className="Intro-text">
                         <h1>Hello!</h1>
                         <h1>My name's Grant, and I am a...</h1>
-                        <h1 className="Intro-title">NONPROFIT COO</h1>
+                        <h1 className="Intro-title">{rolesArray[this.state.roleIndex % 4]}</h1>
                     </div>                          
                 </div>
             </>
         );
+    }
+
+    componentDidMount() {
+        this.myInterval = setInterval(() => {
+            this.setState({
+                roleIndex: this.state.roleIndex + 1
+            });
+        }, 3000);
     }
 }
