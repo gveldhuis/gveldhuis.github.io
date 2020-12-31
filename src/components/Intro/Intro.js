0512d0401bdad5126b6ba60cde860c48b0f26780
gveldhuis <veldhuis@umich.edu>
2020-12-31 11:27:45 -0500

    Standardized Intro padding, margin

--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -9,11 +9,11 @@ export class Intro extends React.Component {
             <>
                 <div id="Intro"></div>
                 <div className="Intro">
-                <div className="Intro-text">
-                     <h1>Hello!</h1>
-                     <h1>My name's Grant, and I am a...</h1>
-                     <h1 className="Intro-title">NONPROFIT COO</h1>
-                </div>                          
+                    <div className="Intro-text">
+                        <h1>Hello!</h1>
+                        <h1>My name's Grant, and I am a...</h1>
+                        <h1 className="Intro-title">NONPROFIT COO</h1>
+                    </div>                          
                 </div>
             </>
         );
