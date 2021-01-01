ff65488e7b27a68c0690c746c2410b21c1e6bd5c
gveldhuis <veldhuis@umich.edu>
2021-01-01 14:19:35 -0500

    Created WorkBlock class instances

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -4,7 +4,34 @@ import '../../../src/index.css'
 import atricureWork from './AtricureWork.png'
 import blueprintsWork from './BlueprintsWork.png'
 
+class WorkBlock {
+    constructor(projectTitle, company, brief, tech, impact, image) {
+        this.projectTitle = projectTitle;
+        this.company = company;
+        this.brief = brief;
+        this.tech = tech;
+        this.impact = impact;
+        this.image = image;
+    }
+}
 
+var inventoryapp = new WorkBlock(
+    "Inventory App",
+    "Blueprints For Pangaea",
+    "I acted as product manager for the development of this mobile web-app and desktop admin console combination, meant to scan product labels to help better inventory medical supplies.",
+    "Mobile web-app uses Tesseract OCR to recognize text in image of scanned label. Desktop admin console uses Django backend connected to sqLite database. Both parts of app use React.js frontend and Microsoft Azure’s App Service.",
+    "$20,000 in University funding, 150% increase in inventorying efficiency.",
+    blueprintsWork,
+);
+
+var atricureapp = new WorkBlock(
+    "Staffing App",
+    "AtriCure",
+    "I developed an interactive, internal web-app to help manufacturing supervisors calculate optimal staffing for production lines, based on current manpower, historical output, and consumer demand.",
+    "Utilized React.js fronted with a Node.js backend, connected to mySQL database.",
+    "85% utilization among supervisors after 2 weeks.",
+    atricureWork,
+);
 
 export class Work extends React.Component {
     render() {
