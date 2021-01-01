02200904c24742c7a47bec85fc54da7a1ef3938b
gveldhuis <veldhuis@umich.edu>
2021-01-01 13:35:08 -0500

    Reduced redundancy, now componsed of ExperienceItems

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -37,7 +37,23 @@ var blueprints = new ExperienceBlock(
      "Leading Operational strategy for 7 national University chapters shipping $1M of medical supplies yearly",
      "Coordinating donation of $12,000 worth of PPE to community organizations amid COVID-19",
     ],
-)
+);
+
+var michiganmedicine = new ExperienceBlock(
+    michiganLogo, 
+    "Michigan Medicine",
+    "Research Assistant",
+    "In this role, I was able to build on my experience in clinical research by collecting patient data for a multi-center study. I also saw firsthand how the inefficiencies of our healthcare system holds back physicians and disadvantages patients. My most notable project involved...",
+    ["Collecting 65 different clinical datapoints for 30+ complex patients, helping find statistically signifiant difference in outcomes for two common heart procedures."],
+);
+
+var childrens = new ExperienceBlock(
+    childrensLogo,
+    "Cincinnati Children's",
+    "Research Assistant",
+    "At one of the top children’s hospitals globally, I had the priviledge of working with patients and surgeons alike to conduct research on a broad array of abnormal heart conditions. My aunt and cousin have suffered heart conditions their entire life, which is why my passion for healthcare began here. My proudest achievement includes...",
+    ["Co-authoring a poster presentation on novel diagnosis for complex heart condition, which was presented at 2019 AHA National Scientific Conference to +12,000 healthcare professionals."],
+);
 
 export class Experience extends React.Component {
     render() {
@@ -49,72 +65,10 @@ export class Experience extends React.Component {
                 <div className="Experience">
                     <div className="Experience-container">
                         <h1 className="Experience-header">Experience.</h1>
-                            <ExperienceItem experienceblock={atricure}/>
-                            <ExperienceItem experienceblock={blueprints}/>
-                        {/*<div className="Experience-block">
-                            <div className="Experience-img"> 
-                                <img src={atricureLogo} alt="AtriCure logo"></img>
-                            </div>
-                            <div className="Experience-text">
-                                <h1 className="Experience-itemheader">AtriCure, Inc.</h1>
-                                <h2 className="Experience-itemrole">Operations Engineering Co-op</h2>
-                                <p className="Experience-description">
-                                    My term at AtriCure was nothing short of amazing. I implemented Lean on a manufacturing floor, developed an internal website to optimize factory staffing, and ultimately helped make a difference in the lives of patients with AFib.  Highlights include...<br /><br />
-
-                                    <b>Implementing Lean which created $70,000 in annual savings</b><br /><br />
-
-                                    <b>Developing Power-BI dashboard with 85% utilization rate</b><br /><br />
-
-                                    <b>Creating React.js web-app to optimize line staffing</b>
-                                </p>
-                            </div>
-                        </div>
-                        <div className="Experience-block">
-                            <div className="Experience-img">
-                                <img src={blueprintsLogo} alt="Blueprints For Pangaea logo"></img>
-                            </div>
-                            <div className="Experience-text">
-                                <h1 className="Experience-itemheader">Blueprints For Pangaea</h1>
-                                <h2 className="Experience-itemrole">COO, Product Manager</h2>
-                                <p className="Experience-description">
-                                    Blueprints is a student-run, 501-(c)(3) nonprofit that ships excess medical supplies overseas. Serving as COO has been the honor of a lifetime, as Blueprints combines my passion for healthcare, love for technology, and drive to create positive efficiency in the world. <br />My favorite moments include...<br /><br />
-
-                                    <b>Acting as Product Manager for internal web-app, winning $20,000 in University funding and creating 150% efficiency increase</b><br /><br />
-
-                                    <b>Leading Operational strategy for 7 national University chapters shipping $1M of medical supplies yearly</b><br /><br />
-
-                                    <b>Coordinating donation of $12,000 worth of PPE to community organizations amid COVID-19</b><br /><br />
-                                </p>
-                            </div>
-                        </div>
-                        <div className="Experience-block">
-                            <div className="Experience-img">
-                                <img src={michiganLogo} alt="Michigan Medicine logo"></img>
-                            </div>
-                            <div className="Experience-text">
-                                <h1 className="Experience-itemheader">Michigan Medicine</h1>
-                                <h2 className="Experience-itemrole">Research Assistant</h2>
-                                <p className="Experience-description">
-                                    In this role, I was able to build on my experience in clinical research by collecting patient data for a multi-center study. I also saw firsthand how the inefficiencies of our healthcare system holds back physicians and disadvantages patients. My most notable project involved...<br /><br />
-
-                                    <b>Collecting 65 different clinical datapoints for 30+ complex patients, helping find statistically signifiant difference in outcomes for two common heart procedures.</b>
-                                </p>
-                            </div>
-                        </div>
-                        <div className="Experience-block">
-                            <div className="Experience-img">
-                                <img src={childrensLogo} alt="Cincinnati Children's logo"></img>
-                            </div>
-                            <div className="Experience-text">
-                                <h1 className="Experience-itemheader">Cincinnati Children's</h1>
-                                <h2 className="Experience-itemrole">Research Assistant</h2>
-                                <p className="Experience-description">
-                                    At one of the top children’s hospitals globally, I had the priviledge of working with patients and surgeons alike to conduct research on a broad array of abnormal heart conditions. My aunt and cousin have suffered heart conditions their entire life, which is why my passion for healthcare began here. My proudest achievement includes...<br /><br />
-
-                                    <b>Co-authoring a poster presentation on novel diagnosis for complex heart condition, which was presented at 2019 AHA National Scientific Conference to +12,000 healthcare professionals.</b>
-                                </p>
-                            </div>
-                        </div>*/}
+                            <ExperienceItem experienceblock={atricure} />
+                            <ExperienceItem experienceblock={blueprints} />
+                            <ExperienceItem experienceblock={michiganmedicine} />
+                            <ExperienceItem experienceblock={childrens} />
                     </div>
                 </div>                
             </>
